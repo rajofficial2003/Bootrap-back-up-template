7f790ce49a065ca3019869c7a7eb8d8cbb989808
P-JAGADISH <spikejaga16@gmail.com>
2024-05-26 08:55:48 +0530

    changes
    y spot sample

--- googlelogin/google-login/script.js
+++ googlelogin/google-login/script.js
@@ -29,7 +29,7 @@ auth.languageCode = 'en';
   googleLogin.addEventListener("click", function(){
     // alert("hi");
     signInWithPopup(auth, provider)
-  .then((result) => {
+  .then((result) => { 
     // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
